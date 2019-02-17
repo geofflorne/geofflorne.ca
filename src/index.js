446d1e4ab619d54e0eb8a3d6dc2c4c647b6e85f0
Geoff Lorne <geoff@geofflorne.ca>
2019-02-16 21:55:55 -0800

    dynamicly add dots, shorten anime function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-function fitElementToParent(el, padding) {
+function fitElementToParent(el) {
 	var timeout = null;
 	function resize() {
 		if (timeout) clearTimeout(timeout);
 		anime.set(el, { scale: 1 });
-		var pad = padding || 0;
 		var parentEl = el.parentNode;
-		var elOffsetWidth = el.offsetWidth - pad;
+		var elOffsetWidth = el.offsetWidth;
 		var parentOffsetWidth = parentEl.offsetWidth;
 		var ratio = parentOffsetWidth / elOffsetWidth;
 		timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
@@ -14,52 +13,54 @@ function fitElementToParent(el, padding) {
 	window.addEventListener("resize", resize);
 }
 
-var advancedStaggeringAnimation = (function() {
-	var staggerVisualizerEl = document.querySelector(".stagger-visualizer");
-	var dotsWrapperEl = staggerVisualizerEl.querySelector(".dots-wrapper");
-	var dotsFragment = document.createDocumentFragment();
-	var grid = [22, 22];
-	var numberOfElements = grid[0] * grid[1];
-	var animation;
-	var paused = true;
+function getViewport() {
+	var viewPortWidth;
+	var viewPortHeight;
 
-	fitElementToParent(staggerVisualizerEl, 9);
+	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+	if (typeof window.innerWidth != "undefined") {
+		(viewPortWidth = window.innerWidth), (viewPortHeight = window.innerHeight);
+	}
+
+	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+	else if (
+		typeof document.documentElement != "undefined" &&
+		typeof document.documentElement.clientWidth != "undefined" &&
+		document.documentElement.clientWidth != 0
+	) {
+		(viewPortWidth = document.documentElement.clientWidth),
+			(viewPortHeight = document.documentElement.clientHeight);
+	}
+
+	// older versions of IE
+	else {
+		(viewPortWidth = document.getElementsByTagName("body")[0].clientWidth),
+			(viewPortHeight = document.getElementsByTagName("body")[0].clientHeight);
+	}
+	return [viewPortWidth, viewPortHeight];
+}
+
+var staggeringAnimation = (function() {
+	var dotsWrapperEl = document.querySelector(".dots-wrapper");
+	var dotsFragment = document.createDocumentFragment();
+	var viewPortSize = getViewport();
+	var grid = [parseInt(viewPortSize[0] / 47), parseInt(viewPortSize[1] / 47)];
+	var numberOfElements = grid[0] * grid[1];
 
 	for (var i = 0; i < numberOfElements; i++) {
 		var dotEl = document.createElement("svg");
 		dotEl.id = i;
 		dotEl.classList.add("dot");
 		dotEl.addEventListener("click", function play(e) {
-			// console.log("play");
-			// console.log(this.id);
-
-			paused = false;
-			if (animation) animation.pause();
-
-			animation = anime
-				.timeline({
-					easing: "easeInOutQuad"
-					// complete: play
-				})
-				.add(
-					{
-						targets: ".stagger-visualizer .dot",
-						keyframes: [
-							{
-								scale: anime.stagger([1, 1], { grid: grid, from: this.id }),
-								background: "#C795AE",
-								duration: 100
-							},
-							{
-								scale: 1,
-								background: "#1f2427",
-								duration: 1200
-							}
-						],
-						delay: anime.stagger(40, { grid: grid, from: this.id })
-					},
-					0
-				);
+			console.log(e);
+			stagger = anime({
+				targets: ".stagger-visualizer .dot",
+				background: [
+					{ value: "#C795AE", easing: "easeOutSine", duration: 500 },
+					{ value: "#1f2427", easing: "easeInOutQuad", duration: 1200 }
+				],
+				delay: anime.stagger(40, { grid: grid, from: this.id })
+			});
 		});
 		dotsFragment.appendChild(dotEl);
 	}
